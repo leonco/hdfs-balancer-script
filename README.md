3216ce16266ff4dc16c90f86ba4b54a64f7467fd
wk180 <57928552+wk180@users.noreply.github.com>
2023-10-07 10:56:21 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hdfs-balancer-script
+
+同一节点的多个盘之间的空间平衡
---
+++ main.ts
@@ -0,0 +1,48 @@
+import $ from "https://deno.land/x/dax/mod.ts"
+
+if(import.meta.main){
+  const nodeName = Deno.args[0]
+  let status = await getPlanStatus(nodeName)
+  if('PLAN_UNDER_PROGRESS' === status){
+    console.log("A plan is running.")
+    Deno.exit(0);
+  }
+  const planFile = await generatePlan(nodeName)
+  if(!planFile){
+    console.log("Generate plan file failed...")
+    Deno.exit(1);
+  }
+  console.log('Plan File: ${planFile}')
+  await executePlan(planFile)
+  status = await getPlanStatus(nodeName)
+  if('PLAN_UNDER_PROGRESS' === status){
+    console.log("The plan runs successfully")
+    Deno.exit(0);
+  }
+  throw new Error(status)
+}
+
+async function generatePlan(nodeName: string): Promise<string | undefined>{
+  const result = await  $`hdfs diskbalancer -plan ${nodeName}`.text()
+  const lines = result.split("\n")
+  const regex = /\/system\/diskbalancer\/(.*?)$/;
+  for(const line of lines){
+    const found = line.match(regex)
+    if(found){
+      return `${found[0]}/${nodeName}.plan.json`
+    }
+  }
+}
+
+async function executePlan(planFile: string): Promise<void> {
+  await $`hdfs diskbalancer -execute ${planFile}`
+}
+
+async function getPlanStatus(nodeName: string): Promise<string> {
+  const result = await $`hdfs diskbalancer -query ${nodeName}`.text();
+  const lines = result.split("\n")
+  const status = lines[2]
+  return status.substr('Result: '.length)
+}
+
+
